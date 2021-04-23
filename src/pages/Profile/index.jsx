1fb184a68dbea91b522f0c4e1cb98afb76039fd2
Fernanda Delleprane <fernandadelleprane@gmail.com>
2021-04-23 19:42:30 -0300

    Created SignUp and Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
+class Profile extends Component {
+   state = {
+      profile: [],
+   }
 
-function Profile () {
-   return <h1>Perfil</h1>
+   //Lembrar de pegar ID no localStorage
+   async componentDidMount() {
+      const response = await api.get('perfil/606d07330b45613d1c9ee0cc');
+      console.log(response)
+      this.setState({ profile: response.data })
+   }
+
+   render() {
+      const { profile } = this.state
+
+      return (
+         <div>
+            <h1>Perfil</h1>
+            {profile.name}
+         </div>
+      );
+   };
 }
 
 export default Profile;
