6a3638beb1397ea4c480d907f8723c1e582e8324
Fernanda Delleprane <fernandadelleprane@gmail.com>
2021-04-29 15:10:14 -0300

    Deleted comment

--- src/pages/Edit/Address/index.jsx
+++ src/pages/Edit/Address/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../../services/api';
-// import { getIdUsuario } from '../../../services/auth';
 
 export default function EditAddress() {
 
