d95b2c88b3b8b519928ac9dc7375b10284976109
Fernanda Delleprane <fernandadelleprane@gmail.com>
2021-04-30 19:55:18 -0300

    Add url heroku and remove comment

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,15 +1,8 @@
 import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5000/",
-// });
-
-// export default api;
-
 class Api {
    constructor() {
       this.api = axios.create({
-         baseURL: "http://localhost:5000",
+         baseURL: "https://loja-ironfit.herokuapp.com/",
       });
    }
 
